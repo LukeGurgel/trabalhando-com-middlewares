c609de4a843f8c9f31d6e81bbd497f97c19a245a
Lucas de Sousa Gurgel <lgurgel@leega.local>
2023-01-31 08:55:57 -0300

    create middlewares and tests passed

--- src/index.js
+++ src/index.js
@@ -11,18 +11,72 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => username === user.username);
+  
+  if (!user) {
+    return response.status(404).json({ error: "User does not exists!" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const {user} = request;
+
+  const {pro, todos} = user;
+
+  if (!pro && todos.length > 9) {
+    return response.status(403).json({error: "User reached max number of todos. It's time to go Pro!"})
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Id is not a valid UUID!" });
+  }
+
+  const user = users.find(user => username === user.username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  const todo = user.todos.find(todo => id === todo.id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find(user => user.id === id);
+  
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
